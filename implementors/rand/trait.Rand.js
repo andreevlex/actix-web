518316360325d1b0c0161e4c9c13e81f1544b68a
Travis CI User <travis@example.org>
2018-01-15 03:43:37 +0000

    Documentation upload

---
+++ implementors/rand/trait.Rand.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = ["impl <a class=\"trait\" href=\"https://docs.rs/rand/0.3/rand/trait.Rand.html\" title=\"trait rand::Rand\">Rand</a> for <a class=\"struct\" href=\"https://doc.rust-lang.org/uuid/uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
